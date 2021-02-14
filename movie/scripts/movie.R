044ac2556440a6279609e70e9892a1d86d67819c
imehrlich <isaac.ehrlich@mail.utoronto.ca>
2021-02-13 21:39:13 -0500

    data and fig updates

--- movie/scripts/movie.R
+++ movie/scripts/movie.R
@@ -1,10 +1,10 @@
 ### Movie Data ###
 
 # Load Movie Functions
-source('scripts/movieFunctions.R', echo=TRUE)
+source('movie/scripts/movieFunctions.R', echo=TRUE)
 
 # Load Movie Data
-movie <- read.csv("data/movie.csv")
+movie <- read.csv("movie/data/movie.csv", header = TRUE)
 
 
 
@@ -52,12 +52,12 @@ plotRemakesRating(remakesNames, "figures/remakesRatingSelect.png")
 
 
 ### INDEX ###
-actor <- "Lindsay Lohan";  findActor(actor)[,1:13]
-director <- "Brian De Palma"; findDirector(director)[,1:13]
-genre <- "animation"; findGenre(genre)[,1:13]
-lang <- "French"; findLanguage(lang)[,1:13]
+actor <- "James Franco";  findActor(actor)[,1:13]
+director <- "Todd Phillips"; findDirector(director)[,1:13]
+genre <- "sport"; findGenre(genre)[,1:13]
+lang <- "Spanish"; findLanguage(lang)[,1:13]
 title <- "The Departed";  findMovie(title)[,1:13]
 remakes <- "Bedtime Story"; findRemakes(remakes)[,1:13]
 series <- "How to Train Your Dragon"; findSeries(series)[,1:13]
-year <- 2004;  findYear(year)[,1:13]
+year <- 2020;  findYear(year)[,1:13]
 n <- 10; lastN(n)[,1:13]
