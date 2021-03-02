831efeb051f8aa9aa9291d690f0de32f9110abd9
imehrlich <isaac.ehrlich@mail.utoronto.ca>
2021-03-02 15:01:26 -0500

    data and fig updates

--- movie/scripts/movie.R
+++ movie/scripts/movie.R
@@ -35,29 +35,29 @@ remakes <- movie[!is.na(movie$remakes),]%>%
 
 names <- c("Pirates of the Caribbean", "Ocean's", "Despicable Me", "Mulan", "Cars", "Finding Nemo")
 seriesNames <- getSeries(names)
-plotSeriesRating(seriesNames, "figures/seriesRatingSelect.png")
+plotSeriesRating(seriesNames, "movie/figures/seriesRatingSelect.png")
 
 seriesLongest <- series[series$series %in% names(sort(table(series$series), decreasing = TRUE)[1:5]),] 
-plotSeriesRating(seriesLongest, "figures/seriesRatingLongest.png")
+plotSeriesRating(seriesLongest, "movie/figures/seriesRatingLongest.png")
 
 
 ## Plot Remakes
 
 # plotRemakesRating() # Plot All Remakes
 
-names <- c("Infernal Affairs", "Le Diner de Cons", "Intouchables")
+names <- c("Infernal Affairs", "Le Diner de Cons", "The Intouchables")
 remakesNames <- getRemakes(names)
-plotRemakesRating(remakesNames, "figures/remakesRatingSelect.png") 
+plotRemakesRating(remakesNames, "movie/figures/remakesRatingSelect.png") 
 
 
 
 ### INDEX ###
-actor <- "James Franco";  findActor(actor)[,1:13]
-director <- "Todd Phillips"; findDirector(director)[,1:13]
+actor <- "Anne Hathaway";  findActor(actor)[,1:13]
+director <- "The Coen Brothers"; findDirector(director)[,1:13]
 genre <- "sport"; findGenre(genre)[,1:13]
 lang <- "Spanish"; findLanguage(lang)[,1:13]
-title <- "The Departed";  findMovie(title)[,1:13]
+title <- "Die Another Day";  findMovie(title)[,1:13]
 remakes <- "Bedtime Story"; findRemakes(remakes)[,1:13]
-series <- "How to Train Your Dragon"; findSeries(series)[,1:13]
-year <- 2020;  findYear(year)[,1:13]
+series <- "James Bond"; findSeries(series)[,1:13]
+year <- 1969;  findYear(year)[,1:13]
 n <- 10; lastN(n)[,1:13]
